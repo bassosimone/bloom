ddbd14e1df32001b4b835fbed92e9281b7f8f156
Simone Basso <bassosimone@gmail.com>
2018-03-27 15:18:32 +0200

    extra.cpp: take note of a TODO

--- bloom_extra/extra.cpp
+++ bloom_extra/extra.cpp
@@ -2,6 +2,9 @@
 
 #include <iostream>
 
+// TODO(bassosimone): the main thing that blows when using mingw-w64 is
+// threading, hence this library should use <thread>.
+
 int bloom_extra_initialize() noexcept {
   std::clog << "bloom_extra_initialize" << std::endl;
   return 0;
